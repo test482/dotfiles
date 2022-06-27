8d77324731b4ed1a291e78e35aa052c6907bc15c
Eliot <eliotjoking@gmail.com>
2022-06-27 17:40:25 +0800

    bashrc: myProxy() default action

--- .config/bash/cli-proxy.sh
+++ .config/bash/cli-proxy.sh
@@ -2,18 +2,20 @@ assignProxy(){
     PROXY_ENV="http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY"
     for envar in $PROXY_ENV
     do
-        export $envar=$1
+        export "$envar"="$1"
     done
-    for envar in "no_proxy NO_PROXY"
+
+    NO_PROXY_ENV="no_proxy NO_PROXY"
+    for envar in $NO_PROXY_ENV
     do
-        export $envar=$2
+        export "$envar"="$2"
     done
 }
 clrProxy(){
     PROXY_ENV="http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY"
     for envar in $PROXY_ENV
     do
-        unset $envar
+        unset "$envar"
     done
 }
 
@@ -22,7 +24,18 @@ myProxy(){
     # user=YourUserName
     # read -p "Password: " -s pass &&  echo -e " "
     # proxy_value="http://$user:$pass@ProxyServerAddress:Port"
-    proxy_value="http://$1"
+    if [ $# == 0 ]
+    then
+        proxy_array=127.0.0.1:1080
+    elif [ $# == 1 ]
+    then
+        proxy_array=$1
+    else
+        echo "Usage: myProxy [ProxyServerAddress:Port]"
+        exit 1
+    fi
+
+    proxy_value="http://$proxy_array"
     no_proxy_value="localhost,127.0.0.1,LocalAddress,LocalDomain.com"
-    assignProxy $proxy_value $no_proxy_value
+    assignProxy "$proxy_value" "$no_proxy_value"
 }
