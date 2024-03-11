ecb7167c4fd84f96616ad61a7a9120b814064589
test482 <eliotjoking@gmail.com>
2024-03-12 01:50:43 +0800

    yazi: follow upstream keybind change

--- .config/yazi/init.lua
+++ .config/yazi/init.lua
@@ -1,4 +1,5 @@
 -- https://yazi-rs.github.io/docs/tips/
+
 -- Show symlink in status bar
 function Status:name()
     local h = cx.active.current.hovered
@@ -20,15 +21,15 @@ function Status:owner()
         return ui.Line {}
     end
 
-    return ui.Line {ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"), ui.Span(":"),
-                    ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"), ui.Span(" ")}
+    return ui.Line { ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"), ui.Span(":"),
+        ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"), ui.Span(" ") }
 end
 
 function Status:render(area)
     self.area = area
 
-    local left = ui.Line {self:mode(), self:size(), self:name()}
-    local right = ui.Line {self:owner(), self:permissions(), self:percentage(), self:position()}
-    return {ui.Paragraph(area, {left}), ui.Paragraph(area, {right}):align(ui.Paragraph.RIGHT),
-            table.unpack(Progress:render(area, right:width()))}
+    local left = ui.Line { self:mode(), self:size(), self:name() }
+    local right = ui.Line { self:owner(), self:permissions(), self:percentage(), self:position() }
+    return { ui.Paragraph(area, { left }), ui.Paragraph(area, { right }):align(ui.Paragraph.RIGHT),
+        table.unpack(Progress:render(area, right:width())) }
 end
