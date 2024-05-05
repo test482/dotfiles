fe0e7ac5ae54e075116c6779f40d3cda10a1549c
test482 <eliotjoking@gmail.com>
2024-05-06 05:26:56 +0800

    cli-proxy: update myProxy() func

--- .config/bash/cli-proxy.sh
+++ .config/bash/cli-proxy.sh
@@ -1,39 +1,43 @@
-assignProxy(){
+assignProxy() {
     PROXY_ENV="http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY"
-    for envar in $PROXY_ENV
-    do
+    for envar in $PROXY_ENV; do
         export "$envar"="$1"
     done
 
     NO_PROXY_ENV="no_proxy NO_PROXY"
-    for envar in $NO_PROXY_ENV
-    do
+    for envar in $NO_PROXY_ENV; do
         export "$envar"="$2"
     done
 }
-clrProxy(){
+clrProxy() {
     PROXY_ENV="http_proxy ftp_proxy https_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY ALL_PROXY"
-    for envar in $PROXY_ENV
-    do
+    for envar in $PROXY_ENV; do
         unset "$envar"
     done
 }
 
 # usage: myProxy 127.0.0.1:7890
-myProxy(){
+myProxy() {
     # user=YourUserName
     # read -p "Password: " -s pass &&  echo -e " "
     # proxy_value="http://$user:$pass@ProxyServerAddress:Port"
-    if [ $# == 0 ]
-    then
-        proxy_array=127.0.0.1:7890
-    elif [ $# == 1 ]
-    then
+    local usage="usage: myProxy [IP:Port]"
+    local default_proxy="127.0.0.1:7890"
+    local proxy_pattern="^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$"
+
+    case $# in
+    0) proxy_array=$default_proxy ;;
+    1) if [[ $1 =~ $proxy_pattern ]]; then
         proxy_array=$1
     else
-        echo "Usage: myProxy [ProxyServerAddress:Port]"
+        echo "$usage"
         return 1
-    fi
+    fi ;;
+    *)
+        echo "$usage"
+        return 1
+        ;;
+    esac
 
     proxy_value="http://$proxy_array"
     no_proxy_value="localhost,127.0.0.1,LocalAddress,LocalDomain.com"
